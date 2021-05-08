5e030cb6c8b6668690c53c38657392e81387ed87
biqqles <biqqles@protonmail.com>
2021-05-08 12:44:33 +0100

    Emoticons: use inline reply

--- src/forumlancer/bot/emoticons.rb
+++ src/forumlancer/bot/emoticons.rb
@@ -3,15 +3,15 @@
 require 'discordrb'
 
 # This module allows the bot to handle Skype emoticons. Messages which contain one emoticon specifier, and nothing else,
-# will prompt the bot to send an animated emoji of that emoticon to the same channel. The emojis behind this are stored
-# in the bot's "home server".
+# will prompt the bot to reply with an animated emoji of that emoticon. The emojis behind this are stored in the bot's
+# "home server".
 module Emoticons
   extend Discordrb::EventContainer
 
   message start_with: '(', end_with: ')' do |event|
     emoji_name = event.message.content[1..-2] # text between brackets
     emoji = emoji_map(event.bot)[emoji_name]
-    event.message.reply(emoji.mention) if emoji # hopefully this will send a quote-reply in future
+    event.message.reply!(emoji.mention) if emoji
   end
 
   def self.emoji_map(bot)
