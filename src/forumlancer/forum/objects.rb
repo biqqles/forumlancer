biqqles/forumlancer

commit b13b965d60f308b6a34928c1ec67cd575067ec64

Switch from Nokogiri to Oga

src/forumlancer/forum/objects.rb
@@ -55,16 +55,15 @@ ForumThread = Struct.new(:full_url, :short_title, :last_user, :last_active) do
   include ForumObject
 
   # Alternative constructor from a ".latestthreads_portal" div.
-  # @param portal [Nokogiri::XML::Element] The ".latestthreads_portal" element.
+  # @param portal [Oga::XML::Element] The ".latestthreads_portal" element.
   # @return [ForumThread] The new ForumThread struct.
   def self.from_portal(portal)
-    thread = portal.at('strong').at('a') # retaining "action=lastpost" is intentional
-
-    metadata = portal.at('span')
-    user = metadata.at('a')
+    thread = portal.at_css('strong a') # retaining "action=lastpost" is intentional
+    metadata = portal.at_css('span')
+    user = metadata.at_css('a')
 
     # a different time format is used for posts that were made before the current day
-    time = metadata.at('span')['title'].then do |t|
+    time = metadata.at_css('span')['title'].then do |t|
       Time.strptime(t, '%m-%d-%Y, %I:%M %p')
     rescue ArgumentError
       Time.strptime(t, '%m-%d-%Y')
@@ -80,7 +79,7 @@ ForumThread = Struct.new(:full_url, :short_title, :last_user, :last_active) do
   end
 
   # The document for the archive of this thread. (memoized).
-  # @return [Nokogiri::HTML::Document]
+  # @return [Oga::XML::Document]
   def archive_doc
     @archive_doc ||= fetch_url(archive_url)
   end
@@ -90,21 +89,21 @@ ForumThread = Struct.new(:full_url, :short_title, :last_user, :last_active) do
   # noinspection RubyNilAnalysis
   def name
     short_title unless short_title.end_with? '...'
-    archive_doc.at_css('#fullversion').at('a').text
+    archive_doc.at_css('#fullversion a').text
   end
 
   # The user who started this thread.
   # noinspection RubyNilAnalysis
   # @return [ForumUser]
   def started_by
-    user = archive_doc.at_css('.author').at('a')
+    user = archive_doc.at_css('.author a')
     ForumUser.new(user['href'], user.text)
   end
 
   # The subforum this thread is in.
   # @return [Subforum]
   def subforum
-    link = archive_doc.css('.navigation').search('a').last
+    link = archive_doc.at_css('.navigation a')
     Subforum.new(link['href'], link.text)
   end
 end

Gemfile
@@ -5,6 +5,6 @@ gem 'discordrb'
 gem 'easy_logging'
 gem 'geode'
 gem 'marble_markdown'
-gem 'nokogiri'
+gem 'oga'
 gem 'rufus-scheduler'
 gem 'tzinfo-data'

src/forumlancer/forum/scanner.rb
@@ -11,7 +11,7 @@
 require 'set'
 require 'open-uri'
 
-require 'nokogiri'
+require 'oga'
 
 require_relative 'objects'
 
@@ -40,9 +40,9 @@ end
 
 # Fetch the document for the page at the given URL.
 # @param url [String] The URL to fetch
-# @return [Nokogiri::HTML::Document] The Nokogiri document.
+# @return [Oga::XML::Document] The Oga document.
 def fetch_url(url)
-  Nokogiri::HTML(URI.parse(url).open)
+  Oga.parse_html(URI.parse(url).open)
 end
 
 FORUM_PORTAL = 'https://discoverygc.com/forums/portal.php'

commit 25802fccfda633f1c26611a94a93e8bf85893abf

Objects: fix ForumThread#subforum
Broken in b13b965d

src/forumlancer/forum/objects.rb
@@ -103,7 +103,7 @@ ForumThread = Struct.new(:full_url, :short_title, :last_user, :last_active) do
   # The subforum this thread is in.
   # @return [Subforum]
   def subforum
-    link = archive_doc.at_css('.navigation a')
+    link = archive_doc.css('.navigation a').last
     Subforum.new(link['href'], link.text)
   end
 end