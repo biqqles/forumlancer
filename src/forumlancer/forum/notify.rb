d9425704e2a8d6f8060e1fb46ce04f0ea57140a8
biqqles <biqqles@pm.me>
2022-06-15 02:36:31 +0100

    Notify: log permissions error in Notification#emit

--- src/forumlancer/forum/notify.rb
+++ src/forumlancer/forum/notify.rb
@@ -7,6 +7,7 @@ require 'set'
 require 'discordrb'
 require 'marble'
 
+require_relative '../bot'
 require_relative '../storage'
 require_relative 'scanner'
 
@@ -19,17 +20,25 @@ Notification = Struct.new(:server_id, :channel_id, :thread, :matched) do
   def emit(bot)
     return unless should_emit?
 
-    bot.channel(channel_id)&.send_embed do |embed|
+    begin
+      bot.channel(channel_id)&.send_embed(&embed)
+    rescue Discordrb::Errors::NoPermission
+      logger.error "Unable to send notification in #{server_id}"
+    else
+      record_emission
+    end
+  end
+
+  def embed
+    proc do |embed|
       embed.title = "✉️  You've got mail"
       embed.description = "New post in #{thread.markdown.bold.italic}\nby #{thread.last_user.markdown.bold}"
-      embed.colour = bot.colour
+      embed.colour = Bot::COLOUR
+      embed.timestamp = thread.last_active
       embed.add_field(name: 'In subforum', value: thread.subforum.markdown)
       embed.add_field(name: 'Started by', value: thread.started_by.markdown)
       embed.add_field(name: 'Matched term', value: matched.inspect)
-      embed.timestamp = thread.last_active
     end
-
-    record_emission
   end
 
   # Whether this notification needs to be emitted.
@@ -67,11 +76,7 @@ def notify(bot)
   all_terms = configs.values.map { |c| c[:watchlist] }.to_set.flatten
 
   notifications = create_notifications(all_terms, configs)
-  notifications.each do |n|
-      n.emit(bot)
-  rescue Discordrb::Errors::NoPermission
-    logger.error 'Permission denied'
-  end
+  notifications.each { |n| n.emit(bot) }
 end
 
 # Create notifications for recent threads.
