a170af38cdfbc7e26b29521a88c28d25078efeec
biqqles <biqqles@pm.me>
2022-07-19 17:23:23 +0100

    Forumlancer: fix deletion of old notification records

--- src/forumlancer.rb
+++ src/forumlancer.rb
@@ -27,12 +27,12 @@ module Forumlancer
   end
 
   # schedule deletion of old notification records
-  scheduler.every '1d' do
+  scheduler.every '6h' do
     logger.info 'Deleting old notifications'
 
     Storage.notifications.open do |table|
       seven_days_ago = (Date.today - 7).to_time
-      table[:past].delete_if do |_, _, time|
+      table[:past].delete_if do |_, time|
         Time.at(time) < seven_days_ago
       end
     end
